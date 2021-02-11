0c1ad0758a735208a5ff2598b9520163e477967d
JuanRomero11 <juan.romero-si@mail.escuelaing.edu.co>
2021-02-10 21:24:01 -0500

    parte 3 falta readme

--- src/Todo.js
+++ src/Todo.js
@@ -2,15 +2,21 @@ import React from 'react';
 
 export class Todo extends React.Component {
 
-   constructor(props) {
+    constructor(props) {
         super(props);
-    }   
+    }
 
     render() {
-        return (  
-        <h2> {this.props.text} {this.props.priority} {this.props.dueDate.toString()} </h2>
+        return (
+          //Add your code here to represent a TODO
+   <div>
+          <h1>{this.props.text}</h1>
+          <h1>{this.props.priority}</h1>
+          <h1>{this.props.dueDate.toString()}</h1>
+   </div>
+
+
         );
     }
 
 }
-
